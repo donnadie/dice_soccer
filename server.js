371aa2513d3dd95d4f5447868338ba875a4d1fc6
Fabián Alfredo Arias <fab.arias@gmail.com>
2026-03-07 17:14:54 -0300

    railway + neon integration 1

--- server.js
+++ server.js
@@ -40,10 +40,7 @@ const pool = new Pool({
 });
 */
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL});
 
 pool.on('connect', () => {
     console.log("Connected to Neon database");
